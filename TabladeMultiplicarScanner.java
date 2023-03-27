f3f38c01ab3fb57ff21cb0fbf30bb39f7b698baf
Santiago Gutierrez <119289025+Santiago1721@users.noreply.github.com>
2023-03-27 13:10:19 +0200

    Update TabladeMultiplicarScanner.java

--- TabladeMultiplicarScanner.java
+++ TabladeMultiplicarScanner.java
@@ -6,6 +6,8 @@ public class TablaMultiplicar {
 
    public static void main(String[] args) throws Exception{
       Scanner lector = new Scanner(System.in);
+      
+      System.out.println("introduce el numero que quieras ver su tabla de multiplicar");
       int n = lector.nextInt();
       for(int i = 5; i<= 12; i++){
          System.out.println(n + " X " + i + " = " + (n*i));
