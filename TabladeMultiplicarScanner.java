564991c19db84fe2e1115a6340c2d9f4148ee1de
Santiago Gutierrez <119289025+Santiago1721@users.noreply.github.com>
2023-03-27 13:09:23 +0200

    Update and rename TabladeMultiplicarAPartirdeNumeroDeterminado.java to TabladeMultiplicarScanner.java

--- TabladeMultiplicarScanner.java
+++ TabladeMultiplicarScanner.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class TablaMultiplicar {
 
    public static void main(String[] args) throws Exception{
-      int n = 4;
+      Scanner lector = new Scanner(System.in);
+      int n = lector.nextInt();
       for(int i = 5; i<= 12; i++){
          System.out.println(n + " X " + i + " = " + (n*i));
       }
