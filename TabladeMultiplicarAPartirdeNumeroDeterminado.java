ffff79675727eec2da68f96c0e70a67708437d0e
Santiago Gutierrez <119289025+Santiago1721@users.noreply.github.com>
2023-03-27 13:04:36 +0200

    Create TabladeMultiplicarAPartirdeNumeroDeterminado.java

---
+++ TabladeMultiplicarAPartirdeNumeroDeterminado.java
@@ -0,0 +1,15 @@
+package Matematicos;
+
+import java.util.Scanner;
+
+public class TablaMultiplicar {
+
+   public static void main(String[] args) throws Exception{
+      int n = 4;
+      for(int i = 5; i<= 12; i++){
+         System.out.println(n + " X " + i + " = " + (n*i));
+      }
+   }
+}
+
+
